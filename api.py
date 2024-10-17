1bf9e68d87e248c624f52ec0ca64a64194b12927
Komal Neupane <komal.npn@10-20-131-177.dynapool.wireless.nyu.edu>
2024-10-17 14:27:53 -0400

    wrote api's funstions hash, fetch and summarize

--- api.py
+++ api.py
@@ -26,3 +26,32 @@ users_collection.create_index([("email", 1)], unique=True)
 users_collection.create_index([("username", 1)], unique=True)
 
 NEWS_API_URL = "https://newsapi.org/v2/top-headlines"
+
+# Password hashing function
+def hash_password(password: str) -> str:
+    return hashlib.sha256(password.encode()).hexdigest()
+
+# Fetch news articles from NewsAPI based on user preferences
+def fetch_news(preferences: UserPreferences) -> List[dict]:
+    params = {
+        'apiKey': NEWS_API_KEY,
+        'country': preferences.country,
+        'category': preferences.category,
+        'language': preferences.language,
+        'pageSize': 10  # Fetch 10 articles
+    }
+    response = requests.get(NEWS_API_URL, params=params)
+    if response.status_code == 200:
+        return response.json().get('articles', [])
+    return []
+
+# Summarize news articles based on user-selected style
+def summarize_article(article: dict, summary_style: str) -> str:
+    if summary_style == 'brief':
+        return f"Title: {article['title']}\nSource: {article['source']['name']}\n"
+    elif summary_style == 'humorous':
+        return f"Title: {article['title']} - Brought to you by the always trustworthy {article['source']['name']}!\n"
+    elif summary_style == 'eli5':
+        return f"This article titled '{article['title']}' from {article['source']['name']} is basically saying: {article['description']}.\n"
+    else:
+        return f"Title: {article['title']}\nSource: {article['source']['name']}\nDescription: {article['description']}\nURL: {article['url']}\n"
